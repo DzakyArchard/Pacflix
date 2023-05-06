e624c25603665495f15ccc014a2b275a0a0bb72c
DzakyArchard <131862035+DzakyArchard@users.noreply.github.com>
2023-05-06 11:41:27 +0700

    Update Coba.py

--- Coba.py
+++ Coba.py
@@ -1,3 +1,7 @@
-print("Hello World")
-x = print(10*5)
+user_do = input("anda ingin melakukan registrasi: Y/N")
+
+if user_do == "Y":
+    print("Lakukan Registrasi")
+else:
+    print("Lakukan Subscribe")
 
